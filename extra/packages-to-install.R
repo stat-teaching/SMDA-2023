01eb7df3ddfe74c35bec3bc7660db5d867bf8971
filippogambarota <filippo.gambarota@gmail.com>
2023-04-25 21:14:17 +0200

    clean

--- extra/packages-to-install.R
+++
@@ -1,5 +0,0 @@
-# install the following packages by running the code
-pksg <- c("effects", "tidyverse", "MASS", 
-          "broom", "reshape2", "ggeffects",
-          "performance", "see", "car", "devtools")
-install.packages(pkgs)
